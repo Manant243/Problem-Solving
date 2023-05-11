a25939d4f2d20b9e8155332cb036d107176982b0
Manant Tyagi <82316089+Manant243@users.noreply.github.com>
2023-05-11 19:37:04 +0530

    Time: 7 ms (77.84%), Space: 10.6 MB (31.46%) - LeetHub

---
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+    map <int,int> cnt;
+    
+    void func(int n, vector<int>current){
+        if(current.size() == n){
+            ans.push_back(current); return;
+        }
+        
+        for(auto it : cnt){
+            if(it.second >= 1){
+                cnt[it.first]--;
+                current.push_back(it.first);
+                func(n, current);
+                cnt[it.first]++;
+                current.pop_back();
+            }
+        }
+    }
+    
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        vector <int> current;
+        
+        for(auto it : nums){
+            cnt[it]++;
+        }
+        
+        int n = nums.size();
+        func(n, current);
+        
+        return ans;
+    }
+};
