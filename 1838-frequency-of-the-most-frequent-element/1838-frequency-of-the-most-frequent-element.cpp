c753c48efd05298f1d2dc2034330c24549d9b353
Manant Tyagi <82316089+Manant243@users.noreply.github.com>
2023-05-19 15:59:10 +0530

    Time: 279 ms (24.03%), Space: 98.9 MB (84.78%) - LeetHub

--- 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.cpp
+++ 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.cpp
@@ -5,16 +5,13 @@ public:
         
         int i = 1, j = 0, ans = 1, n = nums.size();
         while(i < n){
-            long sup = long(nums[i]-nums[i-1])*(i-j);
-            if(sup <= k){
+            if(long(nums[i]-nums[i-1])*(i-j) <= k){
                 k -= (nums[i]-nums[i-1])*(i-j);
                 ans = max(ans, i-j+1);
             }
             else{
-                long sup = long(nums[i]-nums[i-1])*(i-j);
-                while(j < i && sup > k){
+                while(j < i && long(nums[i]-nums[i-1])*(i-j) > k){
                     k += (nums[i-1]-nums[j]); j++;
-                    sup = long(nums[i]-nums[i-1])*(i-j);
                 }
                 if(j == i) i++;
                 continue;
