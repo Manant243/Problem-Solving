ed24b5a00d23714b9996469f59b9d08257af6ef2
Manant Tyagi <82316089+Manant243@users.noreply.github.com>
2023-05-20 17:03:47 +0530

    Time: 94 ms (12.69%), Space: 7.9 MB (84.04%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -2,19 +2,19 @@ class Solution {
 public:
     string minWindow(string s, string t) {
         if(t.size() > s.size()) return "";
-        vector <int> char1(128, 0), char2(128, 0);
+        vector <int> chars(128, 0);
         
         int left = -1, right = -1;
         for(int i = 0; i < int(t.size()); i++){
-            char2[t[i]]++;
+            chars[t[i]]++;
         }
         
         int j = 0, ans = 1e6, n = s.size();
         for(int i = 0; i < n; i++){
-            char1[s[i]]++;
+            chars[s[i]]--;
             while(j < i){
-                if(char1[s[j]] > char2[s[j]]){
-                    char1[s[j]]--; j++;
+                if(chars[s[j]] < 0){
+                    chars[s[j]]++; j++;
                 }
                 else{
                     break;
@@ -22,7 +22,7 @@ public:
             }
             bool ok = true;
             for(int m = 0; m < 128; m++){
-                if(char2[m] > char1[m]){
+                if(chars[m] > 0){
                     ok = false; break;
                 }
             }
