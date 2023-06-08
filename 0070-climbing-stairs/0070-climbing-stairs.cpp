434cdc93ba29203bd700c1ed6e139cd6f6012c35
Manant Tyagi <82316089+Manant243@users.noreply.github.com>
2023-06-08 14:04:44 +0530

    Time: 4 ms (12.16%), Space: 6.2 MB (16.22%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,29 +1,18 @@
 class Solution {
 public:
     
-    vector <int> dp;
-    int dfs(int ind, int n){
-        if(ind == n) return 1;
-        if(dp[ind] != -1) return dp[ind];
-            
-        int ans = 0;
-        if(ind + 1 <= n){
-            ans += dfs(ind+1, n);
-        }
-        
-        if(ind + 2 <= n){
-            ans += dfs(ind+2, n);
-        }
-        
-        return dp[ind] = ans;
-    }
-    
     int climbStairs(int n) {
         int ans = 0;
-        dp.resize(n+1, -1);
-        ans += dfs(1, n);
-        if(n > 1) ans += dfs(2, n);
+        vector <int> dp(n+1, 0);
         
-        return ans;
+        dp[0] = 1;
+        for(int i = 1; i <= n; i++){
+            dp[i] += dp[i-1];
+            if(i-2 >= 0) {
+                dp[i] += dp[i-2];
+            }
+        }
+        
+        return dp[n];
     }
 };
