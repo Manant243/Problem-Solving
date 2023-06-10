ea0d89d2598463d0b1ae62879403c8776cf913ed
Manant Tyagi <82316089+Manant243@users.noreply.github.com>
2023-06-10 09:47:00 +0530

    Time: 122 ms (6.42%), Space: 24.6 MB (11.85%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,25 +1,21 @@
 class Solution {
 public:
     
-    int func(int a, int b, string& text1, string& text2, vector<vector<int>>& dp){
-        if(a >= text1.size() || b >= text2.size()) return 0;
-        if(dp[a][b] != -1) return dp[a][b];
-        
-        int ans = 0;
-        if(text1[a] == text2[b]){
-            ans += func(a+1, b+1, text1, text2, dp)+1;
-        }
-        else{
-            ans += max({func(a+1, b, text1, text2, dp), func(a, b+1, text1, text2, dp), func(a+1, b+1, text1, text2, dp)});
-        }
-        
-        return dp[a][b] = ans;
-    }
-    
     int longestCommonSubsequence(string text1, string text2) {
         int m = text1.size(), n = text2.size();
-        vector<vector<int>> dp(m+1, vector<int>(n+1, -1));
+        vector<vector<int>> dp(m+1, vector<int>(n+1, 0));
         
-        return func(0, 0, text1, text2, dp);
+        for(int i = 1; i <= m; i++){
+            for(int j = 1; j <= n; j++){
+                if(text1[i-1] == text2[j-1]){
+                    dp[i][j] = max(dp[i][j], dp[i-1][j-1]+1);
+                }
+                else{
+                    dp[i][j] = max({dp[i-1][j], dp[i][j-1], dp[i-1][j-1]});
+                }
+            }
+        }
+        
+        return dp[m][n];
     }
 };
