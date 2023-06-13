3c84dd810aa31daeeeca53ce66bb8562192642d3
Manant Tyagi <82316089+Manant243@users.noreply.github.com>
2023-06-13 14:45:28 +0530

    Time: 4 ms (76.26%), Space: 6.5 MB (74.58%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.cpp
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -1,32 +1,28 @@
 class Solution {
 public:
     
-    bool func(int ind, int ind1, int ind2, string& s1, string& s2, string& s3, vector<vector<int>>& dp){
-        if(ind == s3.size()){
-            if(ind1 == s1.size() && ind2 == s2.size()){
-                return 1;
-            }
-            return 0;
-        }
-        
-        if(dp[ind1][ind2] != -1) return dp[ind1][ind2];
-        
-        bool ok  = false;
-        if(ind1 < s1.size() && s1[ind1] == s3[ind]){
-            ok |= func(ind+1, ind1+1, ind2, s1, s2, s3, dp);
-        }
-        if(ind2 < s2.size() && s2[ind2] == s3[ind]){
-            ok |= func(ind+1, ind1, ind2+1, s1, s2, s3, dp);
-        }
-        
-        return dp[ind1][ind2] = ok;
-        
-    }
-    
     bool isInterleave(string s1, string s2, string s3) {
         int a = s1.size(), b = s2.size();
-        vector<vector<int>> dp(a+1, vector<int>(b+1, -1));
+        if(a+b != s3.size()) return false;
         
-        return func(0, 0, 0, s1, s2, s3, dp) | func(0, 0, 0, s1, s2, s3, dp);
+        vector<vector<bool>> dp(a+1, vector<bool>(b+1, 0));
+        
+        dp[0][0] = true;
+        for(int i = 0; i <= a; i++){
+            for(int j = 0; j <= b; j++){
+                if(i == 0 && j == 0) continue;
+                if(i == 0){
+                    dp[i][j] = (dp[i][j-1] && s2[j-1] == s3[i+j-1]);
+                }
+                else if(j == 0){
+                    dp[i][j] = (dp[i-1][j] && s1[i-1] == s3[i+j-1]);
+                }
+                else{
+                    dp[i][j] = (dp[i-1][j] && s1[i-1] == s3[i+j-1]) | (dp[i][j-1] && s2[j-1] == s3[i+j-1]);
+                }
+            }
+        }
+        
+        return dp[a][b];
     }
 };
