b1ba55bd41f0619eb8bd2d3198e5f48a37eb6c0f
Manant Tyagi <82316089+Manant243@users.noreply.github.com>
2023-06-13 09:16:18 +0530

    Time: 93 ms (93.40%), Space: 32.7 MB (47.26%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.cpp
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.cpp
@@ -2,30 +2,21 @@ class Solution {
 public:
     int equalPairs(vector<vector<int>>& grid) {
         int n = grid.size();
-        map <vector<int>, int> rows, columns;
-        vector<vector<int>> row, column;
+        map <vector<int>, int> rows;
         
         for(int i = 0; i < n; i++){
-            vector <int> r, c;
-            for(int j = 0; j < n; j++){
-                r.push_back(grid[i][j]);
-                c.push_back(grid[j][i]);
-            }
-            row.push_back(r);
-            column.push_back(c);
-            rows[r]++; columns[c]++;
+            rows[grid[i]]++;
         }
         
         int ans = 0;
-        for(int i = 0; i < row.size(); i++){
-            vector<int>r = row[i];
-            ans += columns[r];
-            
-            vector<int>c = column[i];
-            ans += rows[c];
+        for(int i = 0; i < n; i++){
+            vector <int> a;
+            for(int j = 0; j < n; j++){
+                a.push_back(grid[j][i]);
+            }
+            ans += rows[a];
         }
         
-        ans /= 2;
         return ans;
     }
 };
