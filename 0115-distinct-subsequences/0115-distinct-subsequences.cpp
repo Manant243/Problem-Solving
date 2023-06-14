810ea8257244fd6a678d987ba7844437e8fa4c6c
Manant Tyagi <82316089+Manant243@users.noreply.github.com>
2023-06-14 14:36:36 +0530

    Time: 49 ms (62.63%), Space: 18.3 MB (74.35%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -2,25 +2,25 @@ class Solution
 {
 public:
     
-    int func(int a, int b, string& s, string& t, vector<vector<int>>& dp){
-        if(b == t.size()) return 1;
-        if(a == s.size()) return 0;
-        if(dp[a][b] != -1) return dp[a][b];
-        
-        int ans = 0;
-        ans += func(a+1, b, s, t, dp);
-        
-        if(s[a] == t[b]){
-            ans += func(a+1, b+1, s, t, dp);
-        }
-        
-        return dp[a][b] = ans;
-    }
-    
     int numDistinct(string &s, string &t){
         int n = s.size(), m = t.size();
-        vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
+        vector<vector<unsigned>> dp(m+1, vector<unsigned>(n+1, 0));
         
-        return func(0, 0, s, t, dp);
+        for(int i = 0; i <= n; i++){
+            dp[0][i] = 1;
+        }
+        
+        for(int i = 1; i <= m; i++){
+            for(int j = 1; j <= n; j++){
+                if(t[i-1] == s[j-1]){
+                    dp[i][j] = dp[i][j-1] + dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j] = dp[i][j-1];
+                }
+            }
+        }
+        
+        return dp[m][n];
     }
 };
