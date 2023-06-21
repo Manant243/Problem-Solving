0ee8b22aa3cdf209642560fcf4fa18f6d8cd64e1
Manant Tyagi <82316089+Manant243@users.noreply.github.com>
2023-06-21 18:06:47 +0530

    Time: 11 ms (37.57%), Space: 13.3 MB (23.08%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,53 +1,44 @@
 class Solution {
+    int dx[4] = {-1, 0, 1, 0};
+    int dy[4] = {0, 1, 0, -1};
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        map <pair<int,int>, bool> vis;
         queue <pair<int,int>> q;
         
         int ans = 0, cnt = 0;
         int m = grid.size(), n = grid[0].size();
+        vector<vector<int>> dis(m, vector<int>(n, -1));
         
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
-                if(grid[i][j] == 2){
-                    q.push({i,j}); cnt++;
-                    vis[{i,j}] = true;
-                }
-                if(grid[i][j] == 0){
-                    cnt++;
+                if(grid[i][j] == 2 || grid[i][j] == 0){
+                    if(grid[i][j] == 2) q.push({i,j});
+                    dis[i][j] = 0;
                 }
             }
         }
         
         while(!q.empty()){
+            pair<int,int> a = q.front();
+            q.pop();
             
-            int s = q.size(); 
-            bool ok = false;
-            
-            while(s--){
-                pair<int,int> k = q.front();
-                int a = k.first, b = k.second; q.pop();
-                
-                if(grid[a][b] == 1) ok = true;
-                if(a+1 < m && grid[a+1][b] == 1 && !vis[{a+1, b}]){
-                    q.push({a+1,b}); vis[{a+1, b}] = true; cnt++;
-                }
-                if(a-1 >= 0 && grid[a-1][b] == 1 && !vis[{a-1, b}]){
-                    q.push({a-1,b}); vis[{a-1, b}] = true; cnt++;
-                }
-                if(b+1 < n && grid[a][b+1] == 1 && !vis[{a, b+1}]){
-                    q.push({a,b+1}); vis[{a, b+1}] = true; cnt++;
-                }
-                if(b-1 >= 0 && grid[a][b-1] == 1 && !vis[{a, b-1}]){
-                    q.push({a,b-1}); vis[{a, b-1}] = true; cnt++;
+            for(int i = 0; i < 4; i++){
+                int x = a.first+dx[i];
+                int y = a.second+dy[i];
+                if(x >= 0 && y >= 0 && x < m && y < n && grid[x][y] == 1 && dis[x][y] == -1){
+                    dis[x][y] = dis[a.first][a.second]+1;
+                    q.push({x, y});
                 }
             }
-            
-            if(ok) ans++;
         }
         
-        if(cnt != m*n){
-            return -1;
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(dis[i][j] == -1) return -1;
+                ans = max(ans, dis[i][j]);
+                cout << dis[i][j] << " ";
+            }
+            cout << "\n";
         }
         
         return ans;
